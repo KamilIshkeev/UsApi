c27f89b19bf060a20188488ebe5b37d3b9ed89ca
222209 <ishkeevkamil@gmail.com>
2025-01-15 13:46:47 +0300

    Добавьте файлы проекта.

---
+++ UserApi/Program.cs
@@ -0,0 +1,36 @@
+using Microsoft.EntityFrameworkCore;
+using UserApi.DataBaseContext;
+using UserApi.Interfaces;
+using UserApi.Service;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<ContextDb>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("TestDbString")), ServiceLifetime.Scoped);
+
+builder.Services.AddScoped<IUsersLoginsService, UserLoginService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
+
---
+++ UserApi/Service/UserLoginService.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using UserApi.DataBaseContext;
+using UserApi.Interfaces;
+using UserApi.Model;
+using UserApi.Requests;
+
+namespace UserApi.Service
+{
+    public class UserLoginService : IUsersLoginsService
+    {
+        private readonly ContextDb _context;
+
+        public UserLoginService(ContextDb context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> GetAllUsersAsync()
+        {
+            var users = await _context.Users.ToListAsync();
+
+            return new OkObjectResult(new
+            {
+                data = new { users = users },
+                status = true
+            });
+        }
+
+
+        public async Task<IActionResult> CreateNewUserAndLoginAsync(CreateNewUserAndLogin newUser)
+        {
+            var user = new Users()
+            {
+                Name = newUser.Name,
+                Descrioption = newUser.Description,
+            };
+
+            await _context.Users.AddAsync(user);
+            await _context.SaveChangesAsync();
+
+            var email = new Emails()
+            {
+                User_id = user.id_User,
+                Email = newUser.Email,
+                Password = newUser.Password,
+            };
+
+            await _context.Emails.AddAsync(email);
+            await _context.SaveChangesAsync();
+
+            return new OkObjectResult(new
+            {
+                status = true
+            });
+        }
+    }
+}
